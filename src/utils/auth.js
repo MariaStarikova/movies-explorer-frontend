d53e5dea3ef89bd1976df7e526fc496c2ce65f86
Starikova Maria <maria050200@gmail.com>
2024-07-02 16:54:41 +0500

    Редактирование README.md

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -1,5 +1,5 @@
-// export const BASE_URL = 'http://localhost:3000';
-export const BASE_URL = 'https://api.movies.nomoredomainswork.ru';
+export const BASE_URL = 'http://localhost:3000';
+// export const BASE_URL = 'https://api.movies.nomoredomainswork.ru';
 
 export const register = (name, email, password) => {
   return fetch(`${BASE_URL}/signup`, {
